eaaeadfb3830b760fc2c63ef5f32c9309ce11de3
hollowdll <pixerchat@gmail.com>
2022-07-20 12:33:58 +0300

    update files

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 // Javascript entry point //
 // Main process //
 
+// Get modules to create the app
+const { app, BrowserWindow } = require("electron");
+
+// Create a window
+const createWindow = function() {
+    const win = new BrowserWindow({ width: 800, height: 600 });
+
+    win.loadFile("index.html");
+}
+
