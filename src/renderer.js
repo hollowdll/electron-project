0d48e75fc5c5347b0366f774e3d7328b6447778f
hollowdll <pixerchat@gmail.com>
2022-08-25 21:03:41 +0300

    delete unnecessary files

--- src/renderer.js
+++
@@ -1,7 +0,0 @@
-//---------------------------------------------------//
-// Interaction with web contents in renderer process //
-//---------------------------------------------------//
-
-"use strict";
-
-// This file is for testing
